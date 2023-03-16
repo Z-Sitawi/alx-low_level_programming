48f9a43cbb924dd1d18b37307ea68308b500c688
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-03-16 12:46:08 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+        //declare string
+        char string[] = "\"Programming is like building a multilingual puzzle";
+        //print string
+        puts(string); 
+        return 0;
+}
