5e7627bcb8c239751be9701d8d7fa3a896b56d95
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-03-27 10:04:46 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - updates the value of the pointer
+ *
+ * @a:the first pointer
+ * @b: the second pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
