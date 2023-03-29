bef97ad1f0d9b1316a2a446018745db01543d842
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-03-29 10:26:46 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,11 +3,12 @@
 #include <string.h>
 
 /**
- * _strcmp - copies a string
+ * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
  *
- * return: void
+ * Return: an integer less than, equal to, or greater than zero if s1 is found,
+ * respectively, to be less than, to match, or be greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
