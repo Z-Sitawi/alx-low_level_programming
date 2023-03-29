f3fba92a12f8909de92c684d10fe66ecd756e5c5
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-03-29 07:49:06 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: 0 (SUCCESS)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (0);
+}
