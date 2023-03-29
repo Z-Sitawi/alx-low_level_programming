fa55d18915eb41fd79dd781bb32186fbb254ad55
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-03-29 10:18:53 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strcmp - copies a string
+ * @s1: first string
+ * @s2: second string
+ *
+ * return: void
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
