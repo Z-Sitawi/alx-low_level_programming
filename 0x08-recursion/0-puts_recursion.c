4cda3cbfc2052c5e07ec60f0b8776ad7e456b621
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-04-05 17:25:03 -0700

    a function that prints a sting

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ *
+ * @s: a string.
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
