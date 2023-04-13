27e5c6eb5191b7de4c95dae9cf1030e42583e6bb
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-04-12 17:55:05 -0700

    concatenate two strings using Dynamic Memory Allocation

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, sl1 = strlen(s1), sl2 = strlen(s2);
+	int size = sl1 + sl2 + 1;
+	char *s = (char *)malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < sl1; i++)
+		s[i] = s1[i];
+
+	for (i = 0; i < sl2; i++)
+		s[sl1 + i] = s2[i];
+	s[sl1 + sl2] = '\0';
+
+	return (s);
+}
