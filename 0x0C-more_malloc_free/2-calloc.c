b729e7323447242ed7995074d33c918688d72923
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-04-14 10:26:28 -0700

    allocates memory for an array, using malloc.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *array = malloc(nmemb * sizeof(size));
+	char *array = malloc(nmemb * sizeof(size));
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
