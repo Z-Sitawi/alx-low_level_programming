3443b30c4d8ca4cf17f6990195e0b621a22c5783
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-04-19 08:26:02 -0700

    a function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,3 @@
-#ifndef PRINT_NAME_H
-#define PRINT_NAME_H
-
 #include <stdio.h>
 #include "function_pointers.h"
 /**
@@ -15,5 +12,3 @@ void print_name(char *name, void (*f)(char *))
 {
 	f(name);
 }
-
-#endif
