21faabb4e94af13932d2df21b18dac96ce487a7b
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-04-20 11:27:27 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,9 +22,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			value = va_arg(numbers, int);
-			printf("%d%s", value, separator);
+			printf("%d", value);
+			if (i != n - 1)
+				printf("%s", separator);
 		}
 
 		printf("\n");
+		va_end(numbers);
 	}
 }
