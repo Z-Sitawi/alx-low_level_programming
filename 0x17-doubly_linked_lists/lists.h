54f09f31ec6bef6bebe86c526362e3d20719c4f6
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-09 12:53:06 -0800

    add 0x17

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,33 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ * Auth: Zakaria Aaichaou
+ */
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+size_t dlistint_len(const dlistint_t *h);
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+#endif /* LISTS_H */
